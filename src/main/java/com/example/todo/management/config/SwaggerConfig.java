2e3cf3a939821bcd6993455085ebbc883b800a4c
Shivakant6387 <shivakantsingh900@gmail.com>
2024-01-27 20:02:21 +0530

    swagger

---
+++ src/main/java/com/example/todo/management/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.example.todo.management.config;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Management API")
+                        .description("Todo management api")
+                        .version("v0.0.1")
+                        .contact(new Contact().name("Shivakant Singh"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Todo management api"));
+    }
+}
