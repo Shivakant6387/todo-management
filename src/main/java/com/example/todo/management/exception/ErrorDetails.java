374193e9874357894811d9f7a992ba2bf31e9df3
Shivakant6387 <shivakantsingh900@gmail.com>
2024-02-01 12:12:39 +0530

    handle exceeption

---
+++ src/main/java/com/example/todo/management/exception/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.example.todo.management.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timeStamp;
+    private String message;
+    private String details;
+}
